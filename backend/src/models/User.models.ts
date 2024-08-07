e4cef2b7a5e7026cfa10b9375b6707737b0bf36e
Mirko Fede <mirko@mirkoeffe.shop>
2024-08-07 15:10:08 +0200

    add dev script

---
+++ backend/src/models/User.models.ts
@@ -0,0 +1,24 @@
+import mongoose, { Schema, model } from "mongoose";
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
+    githubUrl: { type: String },
+    website: { type: String },
+    city: { type: String },
+    country: { type: String },
+    languagesSpoken: [{ type: String }],
+    techStack: [{ type: String }],
+    projectsJoined: [{ type: Schema.Types.ObjectId, ref: "Project" }],
+    projectsSuggested: [{ type: Schema.Types.ObjectId, ref: "Project" }],
+    projectsApplied: [{ type: Schema.Types.ObjectId, ref: "Project" }],
+    dashboardPosts: [{ type: Schema.Types.ObjectId, ref: "Post" }],
+  },
+  {
+    collection: "User",
+    timestamps: true,
+  }
+);
