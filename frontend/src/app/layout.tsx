768b5cedadfc58804fd769b03032a1f981070951
benjaminelliott <benjamin.elliott.be@gmail.com>
2024-08-07 15:31:13 +0200

    changing default layout:

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Germania_One, Inter, Source_Code_Pro } from "next/font/google";
+import "./globals.css";
+import React from "react";
+
+const germania = Germania_One({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-germania",
+});
+const sourceCodePro = Source_Code_Pro({
+  subsets: ["latin"],
+  variable: "--font-source-code-pro",
+});
+
+export const metadata: Metadata = {
+  title: "asocial",
+  description: "A social app for asozial devs",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${germania.variable} font-serif ${sourceCodePro.variable} font-sans`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
