ebb77a82cbfe8d9372bd14e2e27464bc4b26fa13
benjaminelliott <benjamin.elliott.be@gmail.com>
2024-08-07 15:34:08 +0200

    adding react import

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-center h-screen w-screen">
+      <h1 className="font-font--germania text-9xl">asozial</h1>
+    </main>
+  );
+}
