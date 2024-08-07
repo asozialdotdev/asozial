84b761790ec43a29c9dcfdc8debcaa4cf1c089d6
benjaminelliott <benjamin.elliott.be@gmail.com>
2024-08-07 15:51:55 +0200

    creating layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,10 +1,7 @@
 import type { Metadata } from "next";
 import { Germania_One, Inter, Source_Code_Pro } from "next/font/google";
 import "./globals.css";
-<<<<<<< HEAD
 import React from "react";
-=======
->>>>>>> 429888ca30ce10cf12e81a58a870fd046efdad15
 
 const germania = Germania_One({
   subsets: ["latin"],
@@ -27,20 +24,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-<<<<<<< HEAD
-    <html lang="en">
-      <body
-        className={`${germania.variable} font-serif ${sourceCodePro.variable} font-sans`}
-      >
-        {children}
-      </body>
-=======
     <html
       lang="en"
       className={`${germania.variable} font-serif ${sourceCodePro.variable} font-sans`}
     >
       <body>{children}</body>
->>>>>>> 429888ca30ce10cf12e81a58a870fd046efdad15
     </html>
   );
 }
