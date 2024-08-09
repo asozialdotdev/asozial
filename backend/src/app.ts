1bedbe0f327c78729eaff8e26f93c3e51b2bc0e1
Mirko Fede <mirko@mirkoeffe.shop>
2024-08-09 14:06:37 +0200

    add authroutes to app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,6 +5,7 @@ import dashboardRouter from "./routes/index";
 import usersRouter from "./routes/Users.routes";
 import projectsRouter from "./routes/Projects.routes";
 import postRouter from "./routes/Post.routes";
+import githubRouter from "./routes/Auth.routes";
 
 const app = express();
 config(app);
@@ -16,5 +17,7 @@ app.use("/users", usersRouter);
 app.use("/projects", projectsRouter);
 
 app.use("/posts", postRouter);
+
+app.use("/auth", githubRouter);
 
 export default app;
