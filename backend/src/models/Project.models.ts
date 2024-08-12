81351fcc78e5ce5dd923fbc8bff2044583df7d9e
Mirko Fede <mirko@mirkoeffe.shop>
2024-08-12 10:43:02 +0200

    add pitch property on project Schema

--- backend/src/models/Project.models.ts
+++ backend/src/models/Project.models.ts
@@ -4,10 +4,10 @@ const projectSchema = new Schema(
   {
     title: { type: String },
     description: { type: String },
+    pitch: { type: String },
     githubRepo: { type: String },
     techStack: [{ type: String }],
     owner: { type: Schema.Types.ObjectId, ref: "User", required: true },
-    // githubOwner: { type: String },
     membersJoined: [{ type: Schema.Types.ObjectId, ref: "User" }],
     membersApplied: [{ type: Schema.Types.ObjectId, ref: "User" }],
     membersInvited: [{ type: Schema.Types.ObjectId, ref: "User" }],
