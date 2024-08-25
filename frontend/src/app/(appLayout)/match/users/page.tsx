123e13391013dded74599e7ee6a3f8ec46636e8f
benjaminelliott <benjamin.elliott.be@gmail.com>
2024-08-25 20:08:22 +0200

    making match pages

---
+++ frontend/src/app/(appLayout)/match/users/page.tsx
@@ -0,0 +1,14 @@
+import PageContainer from "@/components/common/containers/PageContainer";
+import PageTitle from "@/components/common/ui/PageTitle";
+import React from "react";
+
+function page() {
+  return (
+    <PageContainer>
+      <PageTitle>Match user</PageTitle>
+      INSERT MAGIC CODE HERE
+    </PageContainer>
+  );
+}
+
+export default page;
