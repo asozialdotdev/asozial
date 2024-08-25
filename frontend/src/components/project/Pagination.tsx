d9755d2342e375fd4dfc6d4a35819b6e9405b798
Jose Copeti <jrcopeti@gmail.com>
2024-08-25 02:19:44 +0200

    styled pagination

--- frontend/src/components/project/Pagination.tsx
+++ frontend/src/components/project/Pagination.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Link from "next/link";
 import { usePathname, useSearchParams } from "next/navigation";
+import { Button } from "../ui/button";
 
 type PaginationProps = {
   totalPages: number;
@@ -18,26 +19,40 @@ function Pagination({ totalPages, currentPage }: PaginationProps) {
   };
 
   return (
-    <div className="flex items-center justify-center gap-2">
+    <div className="flex items-center justify-center gap-4 pb-4 -mt-8">
       {currentPage > 1 && (
         <Link href={createPageURL(currentPage - 1)}>
-          <span className="px-3 py-1 border rounded-md">Previous</span>
+          <Button
+            variant="link"
+            className="rounded-md border px-5 py-1 hover:bg-zinc-600 hover:text-light dark:hover:bg-zinc-300 dark:hover:text-dark"
+          >
+            Next
+          </Button>
         </Link>
       )}
       {[...Array(totalPages).keys()].map((_, index) => (
         <Link key={index} href={createPageURL(index + 1)}>
-          <span
-            className={`px-3 py-1 border rounded-md ${
-              currentPage === index + 1 ? "bg-gray-300" : ""
+          <Button
+            size="icon"
+            variant="ghost"
+            className={`rounded-md border px-3 py-1 ${
+              currentPage === index + 1
+                ? "bg-zinc-600 text-light dark:bg-zinc-300 dark:text-dark"
+                : "hover:bg-zinc-600 hover:text-light dark:hover:bg-zinc-300 dark:hover:text-dark"
             }`}
           >
             {index + 1}
-          </span>
+          </Button>
         </Link>
       ))}
       {currentPage < totalPages && (
         <Link href={createPageURL(currentPage + 1)}>
-          <span className="px-3 py-1 border rounded-md">Next</span>
+          <Button
+            variant="link"
+            className="rounded-md border px-5 py-1 hover:bg-zinc-600 hover:text-light dark:hover:bg-zinc-300 dark:hover:text-dark"
+          >
+            Next
+          </Button>
         </Link>
       )}
     </div>
