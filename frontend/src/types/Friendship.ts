4c3b7e29006e8d8fb2962e326e3ece09af9fa4f7
benjaminelliott <benjamin.elliott.be@gmail.com>
2024-08-26 09:56:47 +0200

    redefined user model and schema for clarity

--- frontend/src/types/Friendship.ts
+++ frontend/src/types/Friendship.ts
@@ -3,6 +3,7 @@ import { Types } from "mongoose";
 type Friendship = {
   senderId: Types.ObjectId;
   receiverId: Types.ObjectId;
+  messages?: Types.ObjectId[];
   status: "pending" | "accepted" | "declined";
   createdAt?: Date;
   updatedAt?: Date;
