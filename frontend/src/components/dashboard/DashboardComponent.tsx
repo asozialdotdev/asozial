a1de1bdbf7a0a2c6f8e7483603b7a908603f8a81
benjaminelliott <benjamin.elliott.be@gmail.com>
2024-08-26 15:39:21 +0200

    making model changes for login

--- frontend/src/components/dashboard/DashboardComponent.tsx
+++ frontend/src/components/dashboard/DashboardComponent.tsx
@@ -3,13 +3,19 @@ import DashboardProjects from "./DashboardProjects";
 import DashboardPosts from "./DashboardPosts";
 import DashboardHeader from "./DashboardHeader";
 import DashboardFriends from "./DashboardFriends";
+import { auth } from "@/auth";
+import { checkMembersApplied } from "@/actions/projects.server";
+import RequestsTable from "./RequestsTable";
 
-function DashboardComponent() {
+async function DashboardComponent() {
+  const session = await auth();
+  const username = session?.user?.githubUsername;
+  const projects = await checkMembersApplied();
   return (
     <section className="flex w-full flex-col gap-4">
       <DashboardHeader />
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-        <DashboardProjects />
+        <RequestsTable projects={projects} />
         <DashboardPosts />
         <DashboardFriends />
       </div>
