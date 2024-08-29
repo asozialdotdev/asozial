3d1980fe3786d9021dbd2d5625d3ce4a603d6fef
benjaminelliott <benjamin.elliott.be@gmail.com>
2024-08-29 12:18:56 +0200

    fixing accepted being wrong id

--- frontend/src/components/friendship/FriendshipAcceptedCard.tsx
+++ frontend/src/components/friendship/FriendshipAcceptedCard.tsx
@@ -14,11 +14,11 @@ function FriendshipAcceptedCard({ accepted }: { accepted: Friendship[] }) {
                 className="relative mb-4 w-full border-dashed border-zinc-300 bg-inherit bg-zinc-100 p-4 hover:bg-zinc-200/10 dark:border-zinc-600 dark:bg-inherit dark:bg-zinc-800 dark:shadow-neutral-700/30 dark:hover:bg-zinc-700/10 lg:max-w-[45%]"
               >
                 <DashboardFriendItem
-                  key={friendship.senderId?._id.toString()}
+                  key={friendship.receiverId?._id.toString()}
                   friendshipId={friendship._id}
-                  userId={friendship.senderId?._id}
-                  username={friendship.senderId?.username}
-                  image={friendship.senderId?.info.image}
+                  userId={friendship.receiverId?._id}
+                  username={friendship.receiverId?.username}
+                  image={friendship.receiverId?.info.image}
                   status="accepted"
                 />
               </Card>
