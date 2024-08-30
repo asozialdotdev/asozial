8d7c5926470e69b172cdb63fcf204c2f0b8d118f
Jose Copeti <jrcopeti@gmail.com>
2024-08-30 10:04:20 +0200

    fixed friends tab, included in friends page

--- frontend/src/components/friendship/FriendshipAcceptedCard.tsx
+++ frontend/src/components/friendship/FriendshipAcceptedCard.tsx
@@ -16,7 +16,7 @@ function FriendshipAcceptedCard({
 }: FriendshipAcceptedCardProps) {
   return (
     <>
-      {/* Render Received Accepted Friendships */}
+      {/* Received Accepted Friendships */}
       {receivedAccepted &&
         receivedAccepted.length > 0 &&
         receivedAccepted.map(
@@ -54,7 +54,7 @@ function FriendshipAcceptedCard({
             ),
         )}
 
-      {/* Render Sent Accepted Friendships */}
+      {/* Sent Accepted Friendships */}
       {sentAccepted &&
         sentAccepted.length > 0 &&
         sentAccepted.map(
@@ -62,7 +62,7 @@ function FriendshipAcceptedCard({
             friendship !== null && (
               <Card
                 key={friendship?.senderId?._id?.toString()}
-                className="relative mb-4 w-full border-dashed border-zinc-300 bg-inherit bg-zinc-100 p-4 hover:bg-zinc-200/10 dark:border-zinc-600 dark:bg-inherit dark:bg-zinc-800 dark:shadow-neutral-700/30 dark:hover:bg-zinc-700/10 lg:max-w-[45%]"
+                className="relative mb-4 w-full border-dashed border-zinc-300 bg-inherit bg-zinc-100 p-4 hover:bg-zinc-200/10 dark:border-zinc-600 dark:bg-inherit dark:bg-zinc-800 dark:shadow-neutral-700/30 dark:hover:bg-zinc-700/10 "
               >
                 <DashboardFriendItem
                   key={
