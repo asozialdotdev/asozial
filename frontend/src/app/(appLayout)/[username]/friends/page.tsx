3085f4e6eca1fabbb8f7ab84e9bfbc8182572c2a
Jose Copeti <jrcopeti@gmail.com>
2024-08-30 12:26:13 +0200

    not found components

--- frontend/src/app/(appLayout)/[username]/friends/page.tsx
+++ frontend/src/app/(appLayout)/[username]/friends/page.tsx
@@ -1,6 +1,7 @@
 import { auth } from "@/auth";
 import PageContainer from "@/components/common/containers/PageContainer";
 import ButtonBack from "@/components/common/ui/buttons/ButtonBack";
+import NotFoundComponent from "@/components/common/ui/NotFoundComponent";
 import PageTitle from "@/components/common/ui/PageTitle";
 import FriendStatusTab from "@/components/friendship/FriendStatusTab";
 import React from "react";
@@ -9,18 +10,9 @@ async function page({ params }: { params: { username: string } }) {
   const session = await auth();
   const { username } = params;
 
-  if (session?.user.githubUsername === username) {
+  if (session?.user.githubUsername !== username) {
     return (
-      <PageContainer>
-        <PageTitle>Friends</PageTitle>
-        <p className="text-center text-xl">
-          You are not authorized to view this page.
-        </p>
-        <div className="flex items-center gap-2">
-          <ButtonBack />
-          Go Back
-        </div>
-      </PageContainer>
+      <NotFoundComponent page="Friends" message="You should not be here" />
     );
   }
 
--- frontend/src/app/(appLayout)/[username]/page.tsx
+++ frontend/src/app/(appLayout)/[username]/page.tsx
@@ -1,19 +1,23 @@
-import { getUserFriendStatuses } from "@/actions";
+import { getUserFriendStatuses } from "@/actions/friendships.server/getUserFriendStatuses.server";
 import { getUserByUsername } from "@/actions/users.server/getUserByUsername.server";
 import PageContainer from "@/components/common/containers/PageContainer";
 import UserComponent from "@/components/user/UserComponent";
+import UserComponentSkeleton from "@/components/user/UserComponentSkeleton";
+import { Suspense } from "react";
 
 async function Page({ params }: { params: { username: string } }) {
   const { username } = params;
-  const [result, friends] = await Promise.all([
-    getUserByUsername(username),
-    getUserFriendStatuses(),
-  ]);
+
+  const result = await getUserByUsername(username);
+  const friends = await getUserFriendStatuses();
+
   const { accepted } = friends;
-  
+
   return (
     <PageContainer>
-      <UserComponent result={result} friends={accepted} />
+      <Suspense fallback={<UserComponentSkeleton />}>
+        <UserComponent result={result} friends={accepted} />
+      </Suspense>
     </PageContainer>
   );
 }
